23913f05e50fa1e482debb5971f1a0b02bd8f6f2
linrehui <1571053715@qq.com>
2019-05-07 18:25:43 +0800

    第一次本地提交

---
+++ ABC/src/ABC/ABC.java
@@ -0,0 +1,19 @@
+package ABC;
+
+public class ABC {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	public ABC(int id, String username, String password, int sex, String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.love = love;
+	}
+	
+
+}
